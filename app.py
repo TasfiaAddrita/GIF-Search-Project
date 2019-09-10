558de3c45c3a2b6bc6647adec79583104bccddf4
lauren <ljdutton2@icloud.com>
2019-09-10 11:01:53 -0700

    add api key and url

--- app.py
+++ app.py
@@ -8,10 +8,23 @@ app = Flask(__name__)
 def index():
     """Return homepage."""
     # TODO: Extract query term from url
+r = requests.get("https://api.tenor.com/v1/search?q=%s&key=%s&limit=%s" % (search_term, apikey, lmt)))
 
     # TODO: Make 'params' dict with query term and API key
+params ={
+apikey = 'STTZ6FZ9PGKF'
 
+
+}
     # TODO: Make an API call to Tenor using the 'requests' library
+    # set the apikey and limit
+apikey = 'STTZ6FZ9PGKF'
+lmt = 10
+
+
+
+# continue a similar pattern until the user makes a selection or starts a new search.
+
 
     # TODO: Get the first 10 results from the search results
 
