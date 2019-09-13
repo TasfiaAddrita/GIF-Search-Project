b606ee647bf98994a3118c7f9267414ee874aa42
TasfiaAddrita <tasfia.addrita@gmail.com>
2019-09-13 09:33:20 -0700

    fixed search bar functionality

--- app.py
+++ app.py
@@ -8,12 +8,12 @@ app = Flask(__name__)
 def index():
     """Return homepage."""
     # TODO: Extract query term from url
-    tenor_url = "https://api.tenor.com/v1/search?q=%s&key=%s&limit=%s"
-    submit = request.args.get('submit')
+    # tenor_url = "https://api.tenor.com/v1/search?q=%s&key=%s&limit=%s"
+    # submit = request.args.get('search')
     # TODO: Make 'params' dict with query term and API key
     params = {
         "apikey": 'STTZ6FZ9PGKF',
-        "q": 'cats',
+        "q": "cats",
         "lmt": 1,
         "media": "minimal"
     }
@@ -30,6 +30,25 @@ def index():
         gif = None
 
     # TODO: Render the 'index.html' template, passing the gifs as a named parameter
+    return render_template("index.html", gif=gif)
+
+@app.route('/submit')
+def get_gifs():
+    submit = request.args.get('search')
+    params = {
+        "apikey": 'STTZ6FZ9PGKF',
+        "q": submit,
+        "lmt": 1,
+        "media": "minimal"
+    }
+    r = requests.get("https://api.tenor.com/v1/search?", params=params)
+    if r.status_code == 200:
+        # load the GIFs using the urls for the smaller GIF sizes
+        gif = json.loads(r.content)
+        gif = gif['results'][0]
+    else:
+        gif = None
+
     return render_template("index.html", gif=gif, name=submit)
 
 if __name__ == '__main__':
